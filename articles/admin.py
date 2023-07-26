73657a1f885624f0f220617b4e5f24bf2c541d78
mr6dante6 <92641948+mr6dante6@users.noreply.github.com>
2023-07-26 20:48:48 +0700

    Update admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
 from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_section_count = 0
+        for form in self.forms:
+            is_main = form.cleaned_data.get('is_main')
+            if is_main:
+                main_section_count += 1
+
+        if main_section_count != 1:
+            raise ValidationError('Необходимо указать один основной раздел.')
+
+        return super().clean()
 
 
 class ScopeInline(admin.TabularInline):
     model = Scope
+    formset = ScopeInlineFormset
     extra = 1
 
 
